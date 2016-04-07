24562c89c85ea1bdf05197392b0b01100c3329f8
lqksdsg <394370702@qq.com>
2016-04-07 10:36:43 +0800

    some change

--- src/new.py
+++ src/new.py
@@ -1,10 +1,9 @@
 s = raw_input()
-n,m = s.split(' ')
+n, m = s.split(' ')
 n = int(n)
 m = int(m)
 rules = []
 ips = []
-allow_rules = []
 deny_rules = []
 for i in range(n):
     rule = raw_input()
@@ -17,15 +16,7 @@ for j in range(m):
 def handle_rule(rawrules):
     for z in rawrules:
         tmp = z.split(' ')
-        if tmp[0] == 'allow':
-            ma = handle_netmask(tmp[1])
-            ipt = handle_ip(tmp[1])
-            if ma == 0:
-                result = ipt
-            else:
-                result = ipt[0:ma]
-            allow_rules.append(result)
-        elif tmp[0] == 'deny':
+        if tmp[0] == 'deny':
             ma = handle_netmask(tmp[1])
             ipt = handle_ip(tmp[1])
             if ma == 0:
@@ -34,12 +25,14 @@ def handle_rule(rawrules):
                 result = ipt[0:ma]
             deny_rules.append(result)
 
+
 def handle_netmask(ru):
     t = ru.split('/')
     if len(t) == 2:
         return int(t[1])
     else:
         return 0
+
 
 def handle_ip(Ip):
     Ip_list = Ip.split('/')
@@ -56,12 +49,6 @@ def main():
     handle_rule(rules)
     for p in ips:
         flag = 0
-        # p = handle_ip(p)
-        # for k in allow_rules:
-        #     com = p[0:len(k)]
-        #     if com == k:
-        #         print 'YES'
-
         for x in deny_rules:
             cc = handle_ip(p)[0:len(x)]
             if cc == x:
